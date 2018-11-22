94f4d107785170bed991a96603975b2b906533d5
Chike <chikespott@gmail.com>
2018-11-22 18:22:19 +0100

    initial commit

---
+++ app/src/main/java/com/spark/networks/coding/spark_networks_coding_challenge_chike/MainActivity.java
@@ -0,0 +1,13 @@
+package com.spark.networks.coding.spark_networks_coding_challenge_chike;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
